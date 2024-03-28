18bba55b842761293f9ac48e02494da570e82e0a
MoaWendh <m.wendh@gmail.com>
2024-03-28 18:19:08 -0300

    Desktop Lab, 28/03/24, sexta-feira, 18:18h.

--- src/fStereoTriangulation_moa.m
+++ src/fStereoTriangulation_moa.m
@@ -2,22 +2,38 @@ function [M_L, M_R]= fStereoTriangulation_moa(p_left, p_right, ...
                                               vetorR, t, ... 
                                               f_L, sensorOrigem_L,  distorcaoLente_L, skell_L, ...
                                               f_R, sensorOrigem_R,  distorcaoLente_R, skell_R, ...
-                                              X_L, X_R, pixelSize)
+                                              pixelSize, matrizT_Stereo)
+% p_left_n = normalize_pixel(p_left, f_L, sensorOrigem_L, distorcaoLente_L, skell_L);
+% p_right_n = normalize_pixel(p_right, f_R, sensorOrigem_R, distorcaoLente_R, skell_R);
 
-p_left_n = normalize_pixel(p_left, f_L, sensorOrigem_L, distorcaoLente_L, skell_L);
-p_right_n = normalize_pixel(p_right, f_R, sensorOrigem_R, distorcaoLente_R, skell_R);
+R= [fRodrigues(vetorR) [0; 0; 0]; [0 0 0 1]];
 
-R= fRodrigues(vetorR);
+t= [t ; 1];
 
-baseLine= sqrt(t(1)^2 + t(2)^2 + t(3)^2);
+%baseline= -1*sqrt(t(1)^2 + t(2)^2 + t(3)^2);
 
-disparidade= p_left_n(1) - p_right_n(1);
+% baseline= t(1);
+baseline= matrizT_Stereo(1,4);
 
-z= baseLine/disparidade;
+disparidade= p_right(1) - p_left(1);
 
-x= z*p_left_n(1);
+Z_L= f_L(1)*(baseline/disparidade);
+X_L= (baseline*(p_left(1)-sensorOrigem_L(1))/disparidade);
+Y_L= (baseline*(p_left(2)-sensorOrigem_L(2))/disparidade)*(f_L(1)/f_L(2));
 
-y= z*p_left_n(2);
+disparidade= p_right(1)-p_left(1);
 
+Z_R= f_R(1)*(baseline/disparidade);
+X_R= (baseline*(p_right(1)-sensorOrigem_R(1))/disparidade);
+Y_R= (baseline*(p_right(2)-sensorOrigem_R(2))/disparidade)*(f_R(1)/f_R(2));
+
+P_L=[X_L  Y_L  Z_L  1]';
+P_R= [X_R  Y_R   Z_R  1]';
+
+P_LL= inv(matrizT_Stereo)*P_R;
+P_RR= matrizT_Stereo*P_L;
+
+M_L= P_L(1:3,1);
+M_R= P_R(1:3,1);
 
 end
